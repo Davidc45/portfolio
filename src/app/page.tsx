63aa5e00c5142a1ce618a26e312529afab192a5d
davidc45 <dabidcalderon45@gmail.com>
2025-10-27 13:09:22 -0700

    Resume update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
           >
             Projects
           </button>
-          <a  href="/assets/DavidCalderonResume.pdf" download
+          <a  href="/assets/David_Calderon_Resume.pdf" download
            className="flex items-center space-x-2 border border-gray-600 rounded-lg px-3 py-2 hover:bg-gray-900 hover:border-gray-400 transition-all duration-300 text-sm lg:text-base">
             <span className="hidden sm:inline">Resume</span>
             <span className="sm:hidden">CV</span>
